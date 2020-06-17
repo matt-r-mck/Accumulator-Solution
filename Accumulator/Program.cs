4fd3e59eea12b1702a11e695c841c09937271e4b
matt-r-mck <matt.r.mck@gmail.com>
2020-06-17 16:07:02 -0400

    Add project files.

---
+++ Accumulator/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Accumulator {
+    class Program {
+        static void Main(string[] args) {
+            var total = 0;
+            for  (int num = 0; num <= 50; num++) {
+                if (num % 5 == 0 || num % 7 == 0) //checks if dividible by 5 or 7
+                total += num;
+            }
+            Console.WriteLine($"The Answer is {total}");
+        }
+    }
+}
